1b87984178d422b53c93ed416ac383860774df4a
Alevar <chupick228@gmail.com>
2024-03-27 23:29:42 +0300

    chore: initial setup

---
+++ config/plugins.js
@@ -0,0 +1,23 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "strapi-upload-supabase-provider",
+      providerOptions: {
+        apiUrl: env("SUPABASE_API_URL"),
+        apiKey: env("SUPABASE_API_KEY"),
+        bucket: env("SUPABASE_BUCKET_NAME"),
+        directory: env("SUPABASE_BUCKET_DIRECTORY"),
+      },
+      sizeLimit: 1000000000,
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+        checkFileSize: {},
+      },
+    },
+  },
+  seo: {
+    enabled: true,
+  },
+});
